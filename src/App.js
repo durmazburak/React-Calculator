0a2784f304a90f56e0b08e4653fae8e4f683fb66
Burak DURMAZ <brd536@gmail.com>
2019-03-20 16:38:37 +0300

    calculate design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="calculator">
+        <div className="calculator_display">0</div>
+        <div className="calculator_keys">
+          <button className="key_operator" data-action="add">
+            +
+          </button>
+          <button className="key_operator" data-action="subtract">
+            -
+          </button>
+          <button className="key_operator" data-action="multiply">
+            x
+          </button>
+          <button className="key_operator" data-action="divide">
+            ÷
+          </button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>0</button>
+          <button data-action="decimal">.</button>
+          <button data-action="clear">AC</button>
+          <button class="key_equal" data-action="calculate">
+            =
+          </button>
+        </div>
       </div>
     );
   }
