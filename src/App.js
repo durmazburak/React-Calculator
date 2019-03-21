b77af4b80fbc90817165c8c081571178d4b6ad67
Burak DURMAZ <brd536@gmail.com>
2019-03-22 01:49:05 +0300

    calculation function created

--- src/App.js
+++ src/App.js
@@ -1,36 +1,119 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: "0"
+    };
+  }
+
+  onClickEvent = e => {
+    e.preventDefault();
+    const value = e.target.value; // basılan tuş
+    let result = this.state.result; //ilk baştaki değer  = 0
+    if (result === "0") {
+      result = "";
+      this.setState({ result: result + value });
+    } else if (value === "AC") {
+      let stop = result.slice(0, result.length - 1);
+      this.setState({ result: stop });
+    } else if (value === "=") {
+      /*eslint-disable-next-line*/
+      let output = eval(result);
+      if (output.toString().length >= 10) {
+        output = output.toString().substring(0, 8);
+        this.setState({ result: output });
+      } else {
+        this.setState({ result: output.toString() });
+      }
+    } else {
+      this.setState({ result: result + value });
+    }
+  };
+
   render() {
     return (
       <div className="calculator">
-        <div className="calculator_display">0</div>
+        <div className="calculator_display" id="display">
+          {this.state.result}
+        </div>
         <div className="calculator_keys">
-          <button className="key_operator" data-action="add">
+          <button
+            className="key_operator"
+            onClick={this.onClickEvent}
+            value="+"
+          >
             +
           </button>
-          <button className="key_operator" data-action="subtract">
+          <button
+            className="key_operator"
+            onClick={this.onClickEvent}
+            value="-"
+          >
             -
           </button>
-          <button className="key_operator" data-action="multiply">
+          <button
+            className="key_operator"
+            onClick={this.onClickEvent}
+            value="*"
+          >
             x
           </button>
-          <button className="key_operator" data-action="divide">
+          <button
+            className="key_operator"
+            onClick={this.onClickEvent}
+            value="/"
+          >
             ÷
           </button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>0</button>
-          <button data-action="decimal">.</button>
-          <button data-action="clear">AC</button>
-          <button class="key_equal" data-action="calculate">
+          <button onClick={this.onClickEvent} value="7">
+            {" "}
+            7{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="8">
+            {" "}
+            8{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="9">
+            {" "}
+            9{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="4">
+            {" "}
+            4{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="5">
+            {" "}
+            5{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="6">
+            {" "}
+            6{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="1">
+            {" "}
+            1{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="2">
+            {" "}
+            2{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="3">
+            {" "}
+            3{" "}
+          </button>
+          <button onClick={this.onClickEvent} value="0">
+            {" "}
+            0{" "}
+          </button>
+          <button data-action="decimal" onClick={this.onClickEvent} value=".">
+            .
+          </button>
+          <button data-action="clear" onClick={this.onClickEvent} value="AC">
+            AC
+          </button>
+          <button className="key_equal" onClick={this.onClickEvent} value="=">
             =
           </button>
         </div>
